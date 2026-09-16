768ea013c7f71f92cd33216020512ad466e2aae9
Suryansh Singh <suryanshsingh.codes@gmail.com>
2026-09-15 18:16:52 -1200

    varaible, string, type connversion, operators, conditionals, practice questions

---
+++ Python_Revision/4typeConversion.py
@@ -0,0 +1,22 @@
+# =============================================
+# TYPE CONVERSION
+# =============================================
+print("="*60)
+print("TYPE CONVERSION")
+print("="*60)
+print()
+
+
+myNumber = 8404209856
+print(f"myNumber : {myNumber}")
+print(f"The data type of myNumber : {myNumber} = {type(myNumber)}")
+print()
+
+myNumber=str(myNumber)
+print(f"myNumber : {myNumber}")
+print(f"The data type of myNumber after converting to string : {myNumber} = {type(myNumber)}")
+print()
+
+myNumber=float(myNumber)
+print(f"myNumber : {myNumber}")
+print(f"The data type of myNumber after converting to string : {myNumber} = {type(myNumber)}")
